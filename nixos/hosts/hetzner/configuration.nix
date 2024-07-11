7666bb23605cf12ef39ba60047bd861021966931
hill <hill@hilll.dev>
2024-07-11 17:25:29 +0000

    fixes

--- nixos/hosts/hetzner/configuration.nix
+++ nixos/hosts/hetzner/configuration.nix
@@ -134,23 +134,23 @@ in
                 '';
               };
             };
-            "problem.${domain}" = {
-              forceSSL = true;
-              enableACME = true;
-              locations = {
-                "/" = {
-                  proxyPass = "http://127.0.0.1:8080";
-                  extraConfig = ''
-                    proxy_set_header Host $host;
-                    proxy_set_header X-Real-IP $remote_addr;
-                    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                    proxy_set_header X-Forwarded-Proto $scheme;
-                  '';
-                };
+          };
+          "problem.${domain}" = {
+            forceSSL = true;
+            enableACME = true;
+            locations = {
+              "/" = {
+                proxyPass = "http://127.0.0.1:8080";
+                extraConfig = ''
+                  proxy_set_header Host $host;
+                  proxy_set_header X-Real-IP $remote_addr;
+                  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                  proxy_set_header X-Forwarded-Proto $scheme;
+                '';
               };
             };
-            # TODO: WEBDAV and Syncthing
           };
+          # TODO: WEBDAV and Syncthing
         };
       };
 
