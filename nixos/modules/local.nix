ca178c0185cced08d7e1bc4c70e3947980e02ff3
hill <hill@hilll.dev>
2024-07-22 11:52:20 +0000

    update i18n config

--- nixos/modules/local.nix
+++ nixos/modules/local.nix
@@ -133,7 +133,8 @@
 
   i18n = {
     inputMethod = {
-      enabled = "fcitx5";
+      enabled = true;
+      type = "fcitx5";
       fcitx5.addons = with pkgs; [
         fcitx5-mozc
         fcitx5-chinese-addons
