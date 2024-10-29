1a104b22bc6acd78a18a74fa7fbbf39ee9846fd0
hill <hill@hilll.dev>
2024-10-29 19:00:16 +0000

    stuff

--- nixos/roles/base.nix
+++ nixos/roles/base.nix
@@ -265,7 +265,6 @@
       ports = [22];
       settings = {
         PasswordAuthentication = false;
-        LoginGraceTime = "0";
         UseDns = true;
         X11Forwarding = false;
         PermitRootLogin = "prohibit-password";
