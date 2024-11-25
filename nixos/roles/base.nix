84e6e3615256fa193b7e47e84efc8742fc060994
hill <hill@hilll.dev>
2024-11-25 18:50:27 +0100

    update

--- nixos/roles/base.nix
+++ nixos/roles/base.nix
@@ -74,11 +74,11 @@
   nix = {
     registry = {
       nixpkgs = {
-        flake = inputs.nixpkgs;
+        flake = inputs.nixpkgs-unstable;
       };
     };
     nixPath = [
-      "nixpkgs=${inputs.nixpkgs.outPath}"
+      "nixpkgs=${inputs.nixpkgs-unstable.outPath}"
       "nixos-config=${config.users.users.${username}.home}/.config/nixos/nixos/hosts/${config.networking.hostName}/configuration.nix"
       "/nix/var/nix/profiles/per-user/root/channels"
     ];
