cedddcda49aa8b41e39de2968ba7f7490f6e311f
hill <hill@hilll.dev>
2024-11-25 19:39:52 +0100

    commit

--- flake.nix
+++ flake.nix
@@ -220,6 +220,7 @@
               # hyprland.homeManagerModules.default
               # ./home/linux/hyprland.nix
             ];
+            extraSpecialArgs = inputs;
           };
         };
 
@@ -227,7 +228,20 @@
           default = nix-on-droid.lib.nixOnDroidConfiguration {
             extraSpecialArgs = inputs;
             modules = ["${paths.hosts}/droid.nix"];
+            home-manager-path = home-manager.outPath;
           };
+        };
+
+        docs = pkgs.runCommand "options-doc.md" {} ''
+          cat ${optionsDoc.optionsCommonMark} | ${pkgs.gnused}/bin/sed -E 's|file://||g' | ${pkgs.gnused}/bin/sed -E 's|(\/nix\/store\/[^/]*)\/darwin\/modules|https:\/\/github.com\/float3\/nixos\/tree\/master\/darwin\/modules|g' | ${pkgs.gnused}/bin/sed -E 's|(\/nix\/store\/[^/]*)\/nixos\/modules|https:\/\/github.com\/float3\/nixos\/tree\/master\/nixos\/modules|g' | ${pkgs.gnused}/bin/sed -E 's|(\/nix\/store\/[^/]*)\/home\/modules|https:\/\/github.com\/float3\/nixos\/tree\/master\/home\/modules|g' > $out
+        '';
+
+        devShell = pkgs.mkShell {
+          name = "nixos-configs devShell";
+          buildInputs = with pkgs; [
+            lefthook
+            gitleaks # bug in pkgs.gitleaks currently
+          ];
         };
       };
     });
