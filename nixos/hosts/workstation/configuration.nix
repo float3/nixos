74cce2a6f545ad7fafabced24c7f6edfa32d82c5
hill <hill@hilll.dev>
2024-11-27 15:29:28 +0000

    don't do wayland

--- nixos/hosts/workstation/configuration.nix
+++ nixos/hosts/workstation/configuration.nix
@@ -12,7 +12,6 @@
     "${paths.modules}/local.nix"
     "${paths.vendor}/nvidia.nix"
     "${paths.roles}/vr-passthrough.nix"
-    "${paths.modules}/wayland.nix"
     "${paths.roles}/dev.nix"
     "${paths.roles}/desktop.nix"
   ];
